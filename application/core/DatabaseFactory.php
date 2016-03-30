84976281f252c6ac3683e0d564278d273c658652
Viktor Gobbi <viktor.gobbi@gmail.com>
2016-03-30 07:31:44 +0200

    Fixed connection error

--- application/core/DatabaseFactory.php
+++ application/core/DatabaseFactory.php
@@ -45,7 +45,7 @@ class DatabaseFactory
                 $options = array(PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_OBJ, PDO::ATTR_ERRMODE => PDO::ERRMODE_WARNING);
                 $this->database = new PDO(
                    Config::get('DB_TYPE') . ':host=' . Config::get('DB_HOST') . ';dbname=' .
-                   Config::get('DB_NAME') . ';port=' . Config::get('DB_PORT') . ';charset=' . Config::get('DB_CHARSET'),
+                   Config::get('DB_NAME') . ';port=' . Config::get('DB_PORT'),
                    Config::get('DB_USER'), Config::get('DB_PASS'), $options
                    );
             } catch (PDOException $e) {
