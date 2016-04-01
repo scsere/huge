6ce12be45791aef81995305634ed2fefa3559074
Viktor Gobbi <viktor.gobbi@gmail.com>
2016-04-01 22:28:10 +0200

    Renamed method to prevent conflicts

--- application/core/Feedback.php
+++ application/core/Feedback.php
@@ -5,25 +5,27 @@
  * User: Viktor Gobbi
  * Date: 31/03/16
  * Time: 13.14
+ *
+ * Class for easier feedback messages.
  */
 class Feedback
 {
-    public static function negative($text)
+    public static function addNegative($text)
     {
         Session::add('feedback_negative', $text);
     }
 
-    public static function positive($text)
+    public static function addPositive($text)
     {
         Session::add('feedback_positive', $text);
     }
 
-    public static function warning($text)
+    public static function addWarning($text)
     {
         Session::add('feedback_warning', $text);
     }
 
-    public static function info($text){
+    public static function addInfo($text){
         Session::add('feedback_info', $text);
     }
     
@@ -32,5 +34,9 @@ class Feedback
         Session::set('feedback_info', null);
         Session::set('feedback_negative', null);
         Session::set('feedback_positive', null);
+    }
+
+    public static function resetType($type){
+        Session::set("feedback_$type", null);
     }
 }
