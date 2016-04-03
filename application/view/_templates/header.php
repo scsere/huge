dfee0178dc9a3559561c7fb9daf15bceda34c709
Viktor Gobbi <viktor.gobbi@gmail.com>
2016-04-03 03:35:29 +0200

    Google analytics script in header

--- application/view/_templates/header.php
+++ application/view/_templates/header.php
@@ -12,6 +12,17 @@
     <link rel="stylesheet" href="<?php echo Config::get('URL'); ?>css/style.css" />
 </head>
 <body>
+<?php if (Config::get('GA_ENABLED')): ?>
+        <!-- Google analytics -->
+        <script>
+        (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+        (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+        m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+        })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+        ga('create', '<?php echo Config::get('GA_TRACKING_ID'); ?>', 'auto');
+        ga('send', 'pageview');
+</script>
+    <?php endif; ?>
     <!-- wrapper, to center website -->
     <div class="wrapper">
 
