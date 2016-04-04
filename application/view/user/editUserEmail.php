8309dbfffb7b411a3fa2954b5327d31d0bbd35e0
Viktor Gobbi <viktor.gobbi@gmail.com>
2016-04-04 13:34:16 +0200

    Bootstrap styled user/*

--- application/view/user/editUserEmail.php
+++ application/view/user/editUserEmail.php
@@ -1,17 +1,26 @@
 <div class="container">
-    <h1>UserController/editUserEmail</h1>
+    <div class="page-header">
+        <h1>UserController/editUserEmail</h1>
+    </div>
 
     <!-- echo out the system feedback (error and success messages) -->
     <?php $this->renderFeedbackMessages(); ?>
 
-    <div class="box">
-        <h2>Change your email address</h2>
+    <div class="row">
+        <div class="col-lg-12">
+            <div class="panel panel-default">
+                <div class="panel-body">
+                    <h2>Change your email address</h2>
 
-        <form action="<?php echo Config::get('URL'); ?>user/editUserEmail_action" method="post">
-            <label>
-                New email address: <input type="text" name="user_email" required />
-            </label>
-            <input type="submit" value="Submit" />
-        </form>
+                    <form action="<?php echo Config::get('URL'); ?>user/editUserEmail_action" method="post">
+                        <div class="form-group">
+                            <label for="user_email"> New email address: </label>
+                            <input type="text" class="form-control" name="user_email" id="user_email" required/>
+                        </div>
+                        <input type="submit" class="btn btn-primary" value="Submit"/>
+                    </form>
+                </div>
+            </div>
+        </div>
     </div>
 </div>
