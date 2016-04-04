ff3651004f0f62eb97d00fd6a1c35928945b1912
Viktor Gobbi <viktor.gobbi@gmail.com>
2016-04-04 13:44:32 +0200

    Bootstrap styled dashboard/index

--- application/view/dashboard/index.php
+++ application/view/dashboard/index.php
@@ -1,15 +1,21 @@
 <div class="container">
-    <h1>DashboardController/index</h1>
-    <div class="box">
+    <div class="page-header">
+        <h1>DashboardController/index</h1>
+    </div>
 
-        <!-- echo out the system feedback (error and success messages) -->
-        <?php $this->renderFeedbackMessages(); ?>
+    <!-- echo out the system feedback (error and success messages) -->
+    <?php $this->renderFeedbackMessages(); ?>
 
-        <h3>What happens here ?</h3>
-        <p>
-            This is an area that's only visible for logged in users. Try to log out, an go to /dashboard/ again. You'll
-            be redirected to /index/ as you are not logged in. You can protect a whole section in your app within the
-            according controller by placing <i>Auth::handleLogin();</i> into the constructor.
-        <p>
+    <div class="row">
+        <div class="col-lg-12">
+            <h3>What happens here ?</h3>
+            <p>
+                This is an area that's only visible for logged in users. Try to log out, an go to /dashboard/ again.
+                You'll
+                be redirected to /index/ as you are not logged in. You can protect a whole section in your app within
+                the
+                according controller by placing <i>Auth::handleLogin();</i> into the constructor.
+            <p>
+        </div>
     </div>
 </div>
