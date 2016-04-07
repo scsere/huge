d9511074a03df9ccfe27fadbe3c2fc1263323e55
Viktor Gobbi <viktor.gobbi@gmail.com>
2016-04-07 16:25:34 +0200

    Fixed database type

--- application/_installation/02-create-table-users.sql
+++ application/_installation/02-create-table-users.sql
@@ -12,7 +12,7 @@ CREATE TABLE IF NOT EXISTS users (
   user_creation_timestamp       BIGINT               DEFAULT NULL,
   user_suspension_timestamp     BIGINT               DEFAULT NULL,
   user_last_login_timestamp     BIGINT               DEFAULT NULL,
-  user_failed_logins            BOOLEAN     NOT NULL DEFAULT FALSE,
+  user_failed_logins            INT     NOT NULL DEFAULT 0,
   user_last_failed_login        INT                  DEFAULT NULL,
   user_activation_hash          VARCHAR(40)          DEFAULT NULL,
   user_password_reset_hash      CHAR(40)             DEFAULT NULL,
@@ -26,6 +26,6 @@ INSERT INTO users (user_id,  session_id ,  user_name ,  user_password_hash ,  us
  user_failed_logins ,  user_last_failed_login ,  user_activation_hash ,  user_password_reset_hash ,
  user_password_reset_timestamp ,  user_provider_type ) VALUES
   (1, NULL, 'demo', '$2y$10$OvprunjvKOOhM1h9bzMPs.vuwGIsOqZbw88rzSyGCTJTcE61g5WXi', 'demo@demo.com', TRUE , FALSE , 0, FALSE, NULL,
-      1422205178, NULL, 1422209189, FALSE , NULL, NULL, NULL, NULL, 'DEFAULT'),
+      1422205178, NULL, 1422209189, 0 , NULL, NULL, NULL, NULL, 'DEFAULT'),
   (2, NULL, 'demo2', '$2y$10$OvprunjvKOOhM1h9bzMPs.vuwGIsOqZbw88rzSyGCTJTcE61g5WXi', 'demo2@demo.com', TRUE , FALSE , 1, FALSE , NULL,
-      1422205178, NULL, 1422209189, FALSE , NULL, NULL, NULL, NULL, 'DEFAULT');
+      1422205178, NULL, 1422209189, 0 , NULL, NULL, NULL, NULL, 'DEFAULT');
