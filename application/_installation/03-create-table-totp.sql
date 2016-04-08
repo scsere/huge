24863b449511d8ce912e40a657a0b02065d0866f
Viktor Gobbi <viktor.gobbi@gmail.com>
2016-04-08 06:59:17 +0200

    Added create table sqls for TOTP tables

---
+++ application/_installation/03-create-table-totp.sql
@@ -0,0 +1,22 @@
+START TRANSACTION;
+CREATE TABLE IF NOT EXISTS totp (
+  tid      SERIAL,
+  user_id  INT,
+  tsecret  VARCHAR(256) NOT NULL,
+  tenabled BOOLEAN      NOT NULL,
+
+  CONSTRAINT pk_tid PRIMARY KEY (tid),
+  CONSTRAINT fk_user_id FOREIGN KEY (user_id) REFERENCES users (user_id) MATCH SIMPLE
+);
+
+CREATE TABLE IF NOT EXISTS scratchcodes (
+  scid    SERIAL,
+  tid     INT          NOT NULL,
+  sccode  VARCHAR(128) NOT NULL,
+  scvalid BOOLEAN DEFAULT TRUE,
+  CONSTRAINT pk_scid PRIMARY KEY (scid),
+  CONSTRAINT u_sccode_tid UNIQUE (scid, tid),
+  CONSTRAINT fk_tid FOREIGN KEY (tid) REFERENCES totp (tid) MATCH SIMPLE
+);
+
+COMMIT;
